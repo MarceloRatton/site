68ff786a994d2d0f2775e0af33302176d171f46c
Marcelo Ratton <rattones@marceloratton.com>
2019-07-18 17:59:28 -0300

    recriando o site

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import Navbar from './components/Navbar'
+
+import './App.css'
+import './assets/css/animate.css'
+import './assets/css/bootstrap.min.css'
+import './assets/css/font-awesome.min.css'
+import './assets/css/overwrite.css'
+import './assets/css/simpletextrotator.css'
+import './assets/css/style.css'
+
+
+function App() {
+  return (
+    <div className="App">
+      <h2>MarceloRatton.com</h2>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
